30cd7b6a8ff795865a296d39fe2c30e3b5eb5f9c
Hugh Tollinton <htollinton@hotmail.co.uk>
2021-05-21 11:36:56 +0100

    finished fizzbuzz

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 //let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-let answer = document.getElementById("num").value;
+
 
 
 function fizzy() {
-
+    let answer = document.getElementById("num").value;
+    let display = "hello";
+    let currentNum ="";
 
 for(let i=1 ; i<=answer; i++){
     if((i%3)===0 && (i%5)===0){
-        console.log("FizzBuzz!")
+        console.log("FizzBuzz!");
+        currentNum=  currentNum + " fizzbuzz!";
+        ;
     } else if((i%3)===0){
         console.log("Fizz!");
+        currentNum=  currentNum + " fizz!";
     } else if((i%5)===0){
-        console.log("Buzz!")
+        console.log("Buzz!");
+        currentNum=  currentNum + " buzz!";
     }
     else {
         console.log(i);
+        currentNum=  currentNum + " " + i;
     }
+    document.getElementById("display").innerHTML = currentNum;
 }
-}
-fizzy();
+
+};
+fizzy()
+
+
 
 
